b708b5c5d329b214ab19edb550a9c5f72f262f7c
Elbert <elbert.bae@gmail.com>
2018-10-29 23:30:30 +0000

    setting up the function to get repo

--- download_avatars.js
+++ download_avatars.js
@@ -1,2 +1,10 @@
 var request = require('request');
 
+function getRepoContributors(repoOwner, repoName, callback) {
+
+}
+
+getRepoContributors("https://github.com/nodejs/node", "test2", function (err, result) {
+  console.log('Errors: ', err);
+  console.log('Results: ', result);
+});
