8b988870e34969a6494bd5f236bfa75d70e3f456
Elbert <elbert.bae@gmail.com>
2018-10-29 23:42:13 +0000

    adding header: user-agent as per github API req

--- download_avatars.js
+++ download_avatars.js
@@ -1,7 +1,19 @@
 var request = require('request');
 
-function getRepoContributors(repoOwner, repoName, callback) {
+console.log("Welcome to the Github avatar downloader");
 
+function getRepoContributors(repoOwner, repoName, callback) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    header: {
+      'User-agent': 'request'
+    }
+  };
+
+
+  request(url, function(err, response, body) {
+    callback(err, body);
+  });
 }
 
 getRepoContributors("https://github.com/nodejs/node", "test2", function (err, result) {
